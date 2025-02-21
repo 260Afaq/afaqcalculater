35710ffcdc0480cf9948620b2a28c44401740d03
Afaq Mumtaz <154076060+260Afaq@users.noreply.github.com>
2025-02-21 08:18:25 -0800

    addfilescalculater
    projectcalculater

--- app.js
+++ app.js
@@ -1,104 +1,28 @@
-// // step 1 define quiz data
+// function getvalue(number){
+//     var input=document.getElementById("input")
+//     console.log("getvlaue",number);
+//     input.value= number;
+// }
+function getValue(value) {
+    console.log("getValue", value);
+    var input = document.getElementById("input");
+    input.value += value;
+  }
 
-// const quizData =[
-//     {
-//         question : "what does HTML Stand for?",
-//         options: [
-//             "Hyper text markup language"
-//             "Hyper transfer markup language"
-//             "Hypertext text markup language"
-//             "Hyperlink text markup language"
-//         ],
-//         correct: 0 ,
+  function allClear(){
+    
+    var input = document.getElementById("input")
+    input.value ="";
+  }
 
-//     },
-// {
-//     question:
-//     " In what year did the Great October Socialist Revolution take place?"
-//     option:["1917", "1923", " 1914" ,"1920"]
-//     Correct Answer: 1,
+  function equalTo(){
+    var input = document.getElementById("input");
+    var result = eval(input.value);
+    input.value  = result
+  }
 
-// },
-
-// {
-// question:
-// "What is the largest lake in the world?"
-// option:["Caspian Sea","Baikal","Lake Superior"," Ontario"]
-
-// Correct Answer:2,
-// },
-
-// {
-//     question:"What is the lowest IQ country?"
-//     option:["Nepal","Pakistan","Amrica,","Srilanka"]
-
-//     Correct Answer:1,
-
-// },
-
-// ];
-
-// //step2
-
-// const answerElm = document.querySelectorAll(".answer");
-// const [questionElm, option_1,option_2,option_3,option_4,]= 
-// document.querySelector(
-//     "#question, .option_1, .option_2, .option_3, .option_4,"
-// );
-// const submitBtn = document.querySelector("submit");
-
-// const currentQuiz = 0;
-// const score = 0;
-
-// //Step 3 load quiz fucntion
-
-// const loadQuiz = ()  {
-//     const {question,options} = quizData[currentQuiz];
-//     console.log('question');
-
-//     questionElm.innerText = question
-// };
-
-// loadQuiz();
-
-const item = document.querySelector("#item")
-const toDoBox = document.querySelector("#to-do-box")
-
-item.addEventListener(
-    "keyup",
-    function(event){
-        if(event.key == "Enter"){
-            console.log(this.value)
-            this.value = ""
-        }
-    }
-)
-
-const addToDo = (item)=> {
-    const listItem = document.createElement("li");
-    listItem.innerHTML = `
-      $(item)
-        <i class="fas fa-times"> </i>
-       `;
-
-       listItem.addEventListener(
-        "click",
-        function(){
-            this.classList.toggle("done")
-        }
-       )
-       listItem.querySelector("i")addEventListener(
-        "click",
-        function(){
-            listItem.remove()
-        }
-       )
-
-   toDoBox.appendChild(listitem);
-
-}
-
-
-
-
-
+  function Clear() {
+    var input = document.getElementById("input");
+    input.value = (0);
+    
+  }
